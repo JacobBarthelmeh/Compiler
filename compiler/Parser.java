1926f5681e656150b13d51e9b9ca61cef6260fc1
Jacob Barthelmeh <jacob@wolfssl.com>
2015-02-19 23:37:56 -0700

    update to stubs in parser

--- compiler/Parser.java
+++ compiler/Parser.java
@@ -5,52 +5,217 @@
  */
 package compiler;
 
+import java.io.File;
+import scanner.Scanner;
+
 /**
  *
  * @author sweetness
  */
 public class Parser {
 
+    private volatile Token l1; // look ahead token
+    private volatile Scanner scanner;
+
     /**
-     * See if a token is a match
+     * Set a file to parse
+     *
+     * @param in file to be parsed
+     * @return 0 on success
+     */
+    public int setFile(String in) {
+        try {
+            scanner = new Scanner(in);
+            l1 = scanner.nextToken();
+            return 0;
+        } catch (Exception e) {
+            System.err.println("Error set file " + in + " in parser");
+        }
+        return 1;
+    }
+
+    /**
+     * If match was found consume it and look ahead
      *
      * @param in the current token being looked at
-     * @param toMatch the token compared to
-     * @return true on match
      */
-    private boolean match(Token in, Token toMatch) {
-        if (in == null || toMatch == null) {
+    private void match(Token in) {
+        if (in == null) {
             System.err.println("Invalid input to parser match function");
-            return false;
+            System.exit(1);
         }
-        if (in.getID() == toMatch.getID()) {
-            return true;
-        } else {
-            return false;
-        }
+        l1 = scanner.nextToken();
     }
-    
-    
+
     /**
      * handle error print out
+     *
      * @param err token that caused the error
      * @param expected array of expected tokens
      */
-    private void error(Token err, Token[] expected) {
-        if (err == null || expected == null){
+    private void error(Token err, String[] expected) {
+        if (err == null || expected == null) {
             System.err.println("Improper input to parser error function");
         }
-        System.err.println("Error found " + err.getContents() + " " + 
-                err.getID() + " at line " + err.getLine() + " col " + err.getCol());
+        System.err.println("Error found " + err.getContents() + " "
+                + err.getID() + " at line " + err.getLine() + " col " + err.getCol());
         System.err.print("Was expecting ");
-        for (int i = 0; i < expected.length;i++) {
-            System.err.print(", " + expected[i].getID());
+        for (int i = 0; i < expected.length; i++) {
+            System.err.print(", " + expected[i]);
         }
     }
 
     //stubs for rules 40-47 
-    private void emptyStatement() {
+    private void EmptyStatement() {
 
+    }
+
+    private void ReadStatement() {
+
+    }
+
+    private void ReadParameterTail() {
+
+    }
+
+    private void ReadParameter() {
+
+    }
+
+    private void WriteStatment() {
+
+    }
+
+    private void WriteParameterTail() {
+
+    }
+
+    private void WriteParameter() {
+
+    }
+
+    private void AssignmentStatement() {
+
+    }
+
+    private void IfStatement() {
+
+    }
+
+    private void OptionalElsePart() {
+
+    }
+
+    private void RepeatStatement() {
+
+    }
+
+    private void WhileStatement() {
+
+    }
+
+    private void ForStatement() {
+
+    }
+
+    private void ControlVariable() {
+
+    }
+
+    private void InitialValue() {
+
+    }
+
+    private void StepValue() {
+
+    }
+
+    private void FinalValue() {
+
+    }
+
+    private void ProcedureStatement() {
+
+    }
+
+    private void OptionalActualParameterList() {
+        switch (l1.getID()) {
+            case LPAREN: //rule 68
+                match(l1);
+                ActualParameter();
+                ActualParameterTail();
+                if (l1.getID() == Token.ID.RPAREN) {
+                    match(l1);
+                } else {
+                    String[] err = {")"};
+                    error(l1, err);
+                }
+                break;
+
+            //rule 69 @TODO switch on e
+            default:
+                String[] err = {"("};
+                error(l1, err);
+        }
+    }
+
+    private void ActualParameterTail() {
+        switch (l1.getID()) {
+            case COMMA: //rule 70
+                match(l1);
+                ActualParameter();
+                ActualParameterTail();
+                break;
+
+            // @TODO switch on e
+            default:
+                String[] err = {","};
+                error(l1, err);
+        }
+    }
+
+    private void ActualParameter() {
+        OrdinalEpression();//rule 72
+    }
+
+    private void Expression() {
+        SimpleExpression(); //rule 73
+        OptionalRelationalPart(); //rule 73
+    }
+
+    private void OptionalRelationalPart() {
+        RelationalOperator(); //rule 74
+        SimpleExpression(); //rule 74
+        switch (l1.getID()) {
+            //rule 75
+            //@TODO switch on e
+        }
+    }
+
+    private void RelationalOperator() {
+        switch (l1.getID()) {
+            case EQUAL: //rule 76
+                match(l1);
+                break;
+            case LTHAN: //rule 77
+                match(l1);
+                break;
+            case GTHAN: //rule 78
+                match(l1);
+                break;
+            case LEQUAL: //rule 79
+                match(l1);
+                break;
+            case GEQUAL: //rule 80
+                match(l1);
+                break;
+            case NEQUAL: //rule 81
+                match(l1);
+                break;
+            default:
+                String exp[] = {"EQUAL", "LTHAN", "GTHAN", "LEQUAL", "GEQUAL", "NEQUAL"};
+                error(l1, exp);
+        }
     }
 
     //**************************************************************************
