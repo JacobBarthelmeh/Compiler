e73f0d0b49b1c196d674dfe2c4d4962759b6644f
Todd Beckman <toddbeckman1@gmail.com>
2015-05-01 17:18:54 -0600

    hard-coded the ll1 table

--- compiler/Compiler.java
+++ compiler/Compiler.java
@@ -35,7 +35,7 @@ public class Compiler {
             par.setRuleOutputFile(rOut);
             par.parseFile(fIn);
         } else {
-            Test.run(args);
+            Test.main(args);
         }
     }
 }
